91acd19c51f459fdc73dd2680c3989b1f22f1d1d
tjebbe.lievens@madeit.be <info@madeit.be>
2017-12-09 15:44:39 +0000

    Fix JS style

--- assets/js/customize-preview.js
+++ assets/js/customize-preview.js
@@ -133,7 +133,7 @@
 
     wp.customize( 'show_header_on_sub_pages', function( value ) {
         value.bind( function( to ) {
-            if( false === to ) {
+            if ( false === to ) {
 
                 //Remove image
                 $( '#masthead.site-header.no-front-page' ).hide();
