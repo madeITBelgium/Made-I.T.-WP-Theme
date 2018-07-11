3096e6450bd0c5911e9bed133554143a989c5e0b
tjebbe.lievens@madeit.be <info@madeit.be>
2018-07-11 14:24:52 +0000

    Improvements

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -6,137 +6,137 @@
  *
  * @since 1.0
  */
-if (!function_exists('madeit_posted_on')) :
-/**
- * Prints HTML with meta information for the current post-date/time and author.
- */
-function madeit_posted_on()
-{
+if (!function_exists('madeit_posted_on')) {
+    /**
+     * Prints HTML with meta information for the current post-date/time and author.
+     */
+    function madeit_posted_on()
+    {
 
-    // Get the author name; wrap it in a link.
-    $byline = sprintf(
-        /* translators: %s: post author */
-        __('by %s', 'madeit'),
-        '<span class="author vcard"><a class="url fn n" href="'.esc_url(get_author_posts_url(get_the_author_meta('ID'))).'">'.get_the_author().'</a></span>'
-    );
-
-    // Finally, let's write all of this to the page.
-    echo '<span class="posted-on">'.madeit_time_link().'</span> <span class="byline"> '.$byline.'</span>';
-}
-endif;
-
-if (!function_exists('madeit_time_link')) :
-/**
- * Gets a nicely formatted string for the published date.
- */
-function madeit_time_link()
-{
-    $time_string = '<time class="entry-date published updated" datetime="%1$s">%2$s</time>';
-    if (get_the_time('U') !== get_the_modified_time('U')) {
-        $time_string = '<time class="entry-date published" datetime="%1$s">%2$s</time>';
-    }
-
-    $time_string = sprintf($time_string, get_the_date(DATE_W3C), get_the_date());
-
-    // Wrap the time string in a link, and preface it with 'Posted on'.
-    $result = '<span class="published-on">'.sprintf(
-        /* translators: %s: post date */
-        __('<span>Posted on</span> %s', 'madeit'),
-        '<a href="'.esc_url(get_permalink()).'" rel="bookmark">'.$time_string.'</a>'
-    ).'</span> ';
-
-    if (get_the_time('U') !== get_the_modified_time('U')) {
-        $time_string = '<time class="updated" datetime="%1$s">%2$s</time>';
-
-        $time_string = sprintf($time_string,
-            get_the_modified_date(DATE_W3C),
-            get_the_modified_date()
+        // Get the author name; wrap it in a link.
+        $byline = sprintf(
+            /* translators: %s: post author */
+            __('by %s', 'madeit'),
+            '<span class="author vcard"><a class="url fn n" href="'.esc_url(get_author_posts_url(get_the_author_meta('ID'))).'">'.get_the_author().'</a></span>'
         );
 
-        // Wrap the time string in a link, and preface it with 'Posted on'.
-        $result .= '<span class="updated-on">'.sprintf(
-            /* translators: %s: post date */
-            __('<span>Updated on</span> %s', 'madeit'),
-            '<a href="'.esc_url(get_permalink()).'" rel="bookmark">'.$time_string.'</a>'
-        ).'</span>';
+        // Finally, let's write all of this to the page.
+        echo '<span class="posted-on">'.madeit_time_link().'</span> <span class="byline"> '.$byline.'</span>';
     }
-
-    return $result;
 }
-endif;
 
-if (!function_exists('madeit_entry_footer')) :
-/**
- * Prints HTML with meta information for the categories, tags and comments.
- */
-function madeit_entry_footer()
-{
-    /* translators: used between list items, there is a space after the comma */
-    $separate_meta = __(', ', 'madeit');
-
-    // Get Categories for posts.
-    $categories_list = get_the_category_list($separate_meta);
-
-    // Get Tags for posts.
-    $tags_list = get_the_tag_list('', $separate_meta);
-
-    // We don't want to output .entry-footer if it will be empty, so make sure its not.
-    if (((madeit_categorized_blog() && $categories_list) || $tags_list) || get_edit_post_link()) {
-        echo '<footer class="entry-footer">';
-
-        if ('post' === get_post_type()) {
-            if (($categories_list && madeit_categorized_blog()) || $tags_list) {
-                echo ' <span class="cat-tags-links">';
-
-                // Make sure there's more than one category before displaying.
-                if ($categories_list && madeit_categorized_blog()) {
-                    echo ' <span class="cat-links">'.madeit_get_svg(['icon' => 'folder-open']).'<span class="screen-reader-text">'.__('Categories', 'madeit').'</span>'.$categories_list.'</span>';
-                }
-
-                if ($tags_list && !is_wp_error($tags_list)) {
-                    echo ' <span class="tags-links">'.madeit_get_svg(['icon' => 'hashtag']).'<span class="screen-reader-text">'.__('Tags', 'madeit').'</span>'.$tags_list.'</span>';
-                }
-
-                echo '</span> ';
-            }
+if (!function_exists('madeit_time_link')) {
+    /**
+     * Gets a nicely formatted string for the published date.
+     */
+    function madeit_time_link()
+    {
+        $time_string = '<time class="entry-date published updated" datetime="%1$s">%2$s</time>';
+        if (get_the_time('U') !== get_the_modified_time('U')) {
+            $time_string = '<time class="entry-date published" datetime="%1$s">%2$s</time>';
         }
-        wp_link_pages([
-            'before'            => '<div class="page-links">'.__('Pages:', 'madeit'),
-            'after'             => '</div>',
-            'link_before'       => '<span>',
-            'link_after'        => '</span>',
-            'pagelink'          => '%',
-            'echo'              => 1, ]);
 
-        madeit_edit_link();
+        $time_string = sprintf($time_string, get_the_date(DATE_W3C), get_the_date());
 
-        echo '</footer> <!-- .entry-footer -->';
+        // Wrap the time string in a link, and preface it with 'Posted on'.
+        $result = '<span class="published-on">'.sprintf(
+            /* translators: %s: post date */
+            __('<span>Posted on</span> %s', 'madeit'),
+            '<a href="'.esc_url(get_permalink()).'" rel="bookmark">'.$time_string.'</a>'
+        ).'</span> ';
+
+        if (get_the_time('U') !== get_the_modified_time('U')) {
+            $time_string = '<time class="updated" datetime="%1$s">%2$s</time>';
+
+            $time_string = sprintf($time_string,
+                get_the_modified_date(DATE_W3C),
+                get_the_modified_date()
+            );
+
+            // Wrap the time string in a link, and preface it with 'Posted on'.
+            $result .= '<span class="updated-on">'.sprintf(
+                /* translators: %s: post date */
+                __('<span>Updated on</span> %s', 'madeit'),
+                '<a href="'.esc_url(get_permalink()).'" rel="bookmark">'.$time_string.'</a>'
+            ).'</span>';
+        }
+
+        return $result;
     }
 }
-endif;
 
-if (!function_exists('madeit_edit_link')) :
-/**
- * Returns an accessibility-friendly link to edit a post or page.
- *
- * This also gives us a little context about what exactly we're editing
- * (post or page?) so that users understand a bit more where they are in terms
- * of the template hierarchy and their content. Helpful when/if the single-page
- * layout with multiple posts/pages shown gets confusing.
- */
-function madeit_edit_link()
-{
-    edit_post_link(
-        sprintf(
-            /* translators: %s: Name of current post */
-            __('Edit<span class="screen-reader-text"> "%s"</span>', 'madeit'),
-            get_the_title()
-        ),
-        '<span class="edit-link">',
-        '</span>'
-    );
+if (!function_exists('madeit_entry_footer')) {
+    /**
+     * Prints HTML with meta information for the categories, tags and comments.
+     */
+    function madeit_entry_footer()
+    {
+        /* translators: used between list items, there is a space after the comma */
+        $separate_meta = __(', ', 'madeit');
+
+        // Get Categories for posts.
+        $categories_list = get_the_category_list($separate_meta);
+
+        // Get Tags for posts.
+        $tags_list = get_the_tag_list('', $separate_meta);
+
+        // We don't want to output .entry-footer if it will be empty, so make sure its not.
+        if (((madeit_categorized_blog() && $categories_list) || $tags_list) || get_edit_post_link()) {
+            echo '<footer class="entry-footer">';
+
+            if ('post' === get_post_type()) {
+                if (($categories_list && madeit_categorized_blog()) || $tags_list) {
+                    echo ' <span class="cat-tags-links">';
+
+                    // Make sure there's more than one category before displaying.
+                    if ($categories_list && madeit_categorized_blog()) {
+                        echo ' <span class="cat-links">'.madeit_get_svg(['icon' => 'folder-open']).'<span class="screen-reader-text">'.__('Categories', 'madeit').'</span>'.$categories_list.'</span>';
+                    }
+
+                    if ($tags_list && !is_wp_error($tags_list)) {
+                        echo ' <span class="tags-links">'.madeit_get_svg(['icon' => 'hashtag']).'<span class="screen-reader-text">'.__('Tags', 'madeit').'</span>'.$tags_list.'</span>';
+                    }
+
+                    echo '</span> ';
+                }
+            }
+            wp_link_pages([
+                'before'            => '<div class="page-links">'.__('Pages:', 'madeit'),
+                'after'             => '</div>',
+                'link_before'       => '<span>',
+                'link_after'        => '</span>',
+                'pagelink'          => '%',
+                'echo'              => 1, ]);
+
+            madeit_edit_link();
+
+            echo '</footer> <!-- .entry-footer -->';
+        }
+    }
 }
-endif;
+
+if (!function_exists('madeit_edit_link')) {
+    /**
+     * Returns an accessibility-friendly link to edit a post or page.
+     *
+     * This also gives us a little context about what exactly we're editing
+     * (post or page?) so that users understand a bit more where they are in terms
+     * of the template hierarchy and their content. Helpful when/if the single-page
+     * layout with multiple posts/pages shown gets confusing.
+     */
+    function madeit_edit_link()
+    {
+        edit_post_link(
+            sprintf(
+                /* translators: %s: Name of current post */
+                __('Edit<span class="screen-reader-text"> "%s"</span>', 'madeit'),
+                get_the_title()
+            ),
+            '<span class="edit-link">',
+            '</span>'
+        );
+    }
+}
 
 /**
  * Display a front page section.
