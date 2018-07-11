d8e8d6ffe7c46d40c05069573bff6957be4651e5
tjebbe.lievens@madeit.be <info@madeit.be>
2018-07-11 14:24:42 +0000

    Add comment checkbox for GDPR

--- inc/template-functions.php
+++ inc/template-functions.php
@@ -125,8 +125,8 @@ function madeit_comment($comment, $args, $depth)
 			<footer class="comment-meta">
 				<div class="comment-author vcard">
 					<?php if (0 != $args['avatar_size']) {
-        echo get_avatar($comment, $args['avatar_size']);
-    } ?>
+                        echo get_avatar($comment, $args['avatar_size']);
+                    } ?>
 					<?php printf(__('%s <span class="says">:</span>', 'madeit'), sprintf('<cite class="fn">%s</cite>', get_comment_author_link())); ?>
 				</div><!-- .comment-author -->
 
