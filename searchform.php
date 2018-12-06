553bf87e2b5cf9744aaa355a310e3e21a4f25967
tjebbe.lievens@madeit.be <info@madeit.be>
2018-12-06 15:48:42 +0000

    #89 - search form

--- searchform.php
+++ searchform.php
@@ -13,9 +13,9 @@
 <form method="get" role="search" class="form-search" action="<?php echo esc_url(home_url('/')); ?>">
     <div class="form-group">
         <div class="input-group">
-            <input type="text" id="<?php echo $unique_id; ?>" class="form-control search-query" placeholder="<?php _e('Search...', 'madeit'); ?>" value="<?php echo esc_attr(get_search_query()); ?>" name="s">
+            <input type="text" id="<?php echo $unique_id; ?>" class="form-control search-query" placeholder="<?php _e('Search...', 'madeit'); ?>" aria-label="<?php _e('Search', 'madeit'); ?>" value="<?php echo esc_attr(get_search_query()); ?>" name="s">
             <div class="input-group-append">
-                <button type="submit" class="btn btn-primary" name="submit" id="searchsubmit" value="<?php _e('Search', 'madeit'); ?>"><span class="fa fa-search"></span></button>
+                <button type="submit" class="btn btn-primary" name="submit" id="searchsubmit" aria-label="<?php _e('Search', 'madeit'); ?>" value="<?php _e('Search', 'madeit'); ?>"><span class="fa fa-search"></span></button>
             </div>
         </div>
     </div>
