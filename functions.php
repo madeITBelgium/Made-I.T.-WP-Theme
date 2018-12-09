154bad75013c257f97b503673701d6d5bc0a99e9
tjebbe.lievens@madeit.be <info@madeit.be>
2018-12-09 22:20:49 +0000

    Fix editor & edit location linkÃ

--- functions.php
+++ functions.php
@@ -127,7 +127,7 @@ if (!function_exists('madeit_setup')) {
          * This theme styles the visual editor to resemble the theme style,
          * specifically font, colors, and column width.
          */
-        add_editor_style(['assets/css/editor-style.css', madeit_fonts_url()]);
+        //add_editor_style(['assets/css/editor-style.css', madeit_fonts_url()]);
 
         // Define and register starter content to showcase the theme on new sites.
         $starter_content = [
