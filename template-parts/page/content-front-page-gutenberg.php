4f397fa7e8e851991d7a896d81c261fbc71744ac
madeITBelgium <info@madeit.be>
2018-12-09 22:21:27 +0000

    Apply fixes from StyleCI
    [ci skip] [skip ci]

--- template-parts/page/content-front-page-gutenberg.php
+++ template-parts/page/content-front-page-gutenberg.php
@@ -18,9 +18,8 @@
                     <?php if (has_post_thumbnail()) :
                         $thumbnail = wp_get_attachment_image_src(get_post_thumbnail_id($post->ID), 'madeit-featured-image');
 
-                        // Calculate aspect ratio: h / w * 100%.
-                        $ratio = $thumbnail[2] / $thumbnail[1] * 100;
-                        ?>
+        // Calculate aspect ratio: h / w * 100%.
+        $ratio = $thumbnail[2] / $thumbnail[1] * 100; ?>
 
                         <div class="panel-image" style="background-image: url(<?php echo esc_url($thumbnail[0]); ?>);">
                             <div class="panel-image-prop" style="padding-top: <?php echo esc_attr($ratio); ?>%"></div>
@@ -38,7 +37,7 @@
     <?php
     }
     the_content();
-    
+
     if (!(empty($hidetitle) || $hidetitle == 0)) {
         madeit_edit_link(get_the_ID());
     }
