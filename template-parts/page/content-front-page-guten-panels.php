116d883f468c49a70b87626436cd66d8c4384356
madeITBelgium <info@madeit.be>
2019-05-20 21:07:31 +0000

    Apply fixes from StyleCI
    [ci skip] [skip ci]

--- template-parts/page/content-front-page-guten-panels.php
+++ template-parts/page/content-front-page-guten-panels.php
@@ -19,8 +19,8 @@ global $madeitcounter;
                     <?php if (has_post_thumbnail()) :
                         $thumbnail = wp_get_attachment_image_src(get_post_thumbnail_id($post->ID), 'madeit-featured-image');
 
-                        // Calculate aspect ratio: h / w * 100%.
-                        $ratio = $thumbnail[2] / $thumbnail[1] * 100; ?>
+        // Calculate aspect ratio: h / w * 100%.
+        $ratio = $thumbnail[2] / $thumbnail[1] * 100; ?>
 
                         <div class="panel-image" style="background-image: url(<?php echo esc_url($thumbnail[0]); ?>);">
                             <div class="panel-image-prop" style="padding-top: <?php echo esc_attr($ratio); ?>%"></div>
@@ -37,7 +37,7 @@ global $madeitcounter;
         </div>
     <?php
     }
-    
+
     the_content();
     ?>
 
