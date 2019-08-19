2b5daee585c371c4fc918d07b23de298059ef93d
tjebbe.lievens@madeit.be <info@madeit.be>
2019-08-19 21:42:16 +0000

    Add privacy link

--- template-parts/footer/site-info.php
+++ template-parts/footer/site-info.php
@@ -7,4 +7,4 @@
  * @version 1.0
  */
 ?>
-&copy; <?php echo date('Y'); ?> <?php echo get_bloginfo('name') ?> - <a href="<?php echo esc_url(__('https://www.madeit.be/', 'madeit')); ?>" title="<?php printf(__('Webdesign door %s uit Geel', 'madeit'), 'Made I.T.'); ?>"><?php printf(__('Proudly powered by %s', 'madeit'), 'Made I.T.'); ?></a>
+&copy; <?php echo date('Y'); ?> <?php echo get_bloginfo('name') ?> - <a href="<?php echo get_privacy_policy_url(); ?>"><?php _e('Privacy policy', 'madeit'); ?></a> - <a href="<?php echo esc_url(__('https://www.madeit.be/', 'madeit')); ?>" title="<?php printf(__('Webdesign door %s uit Geel', 'madeit'), 'Made I.T.'); ?>"><?php printf(__('Proudly powered by %s', 'madeit'), 'Made I.T.'); ?></a>
