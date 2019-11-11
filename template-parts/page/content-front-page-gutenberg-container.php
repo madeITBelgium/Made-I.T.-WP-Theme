007084d34214c99cb7ba45ab49554efe23573440
tjebbe.lievens@madeit.be <info@madeit.be>
2019-11-11 18:44:41 +0000

    Fix bugs

--- template-parts/page/content-front-page-gutenberg-container.php
+++ template-parts/page/content-front-page-gutenberg-container.php
@@ -26,7 +26,7 @@
                         </div><!-- .panel-image -->
                     <?php endif; ?>
                     <header class="entry-header">
-                        <?php the_title('<h2 class="entry-title">', '</h2> '); ?>
+                        <?php the_title('<h1 class="entry-title">', '</h1> '); ?>
 
                         <?php madeit_edit_link(get_the_ID()); ?>
 
