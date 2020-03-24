4fe0806df234be821a885cefc49b82c7a2123383
madeITBelgium <info@madeit.be>
2020-03-24 18:29:19 +0000

    Apply fixes from StyleCI
    [ci skip] [skip ci]

--- inc/woocommerce.php
+++ inc/woocommerce.php
@@ -1,7 +1,8 @@
     <?php
 
     if (!function_exists('madeit_loop_columns')) {
-        function madeit_loop_columns() {
+        function madeit_loop_columns()
+        {
             return 3; // 3 products per row
         }
         add_filter('loop_shop_columns', 'madeit_loop_columns', 999);
