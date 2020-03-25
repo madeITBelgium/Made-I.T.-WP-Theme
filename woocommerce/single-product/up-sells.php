ffbcc4d0f0f89d81b653cd9c67b46966828e2a41
Made I.T <info@madeit.be>
2020-03-25 10:05:31 +0100

    Update up-sells.php

--- woocommerce/single-product/up-sells.php
+++ woocommerce/single-product/up-sells.php
@@ -1,51 +1,51 @@
-    <?php
-    /**
-     * Single Product Up-Sells.
-     *
-     * This template can be overridden by copying it to yourtheme/woocommerce/single-product/up-sells.php.
-     *
-     * HOWEVER, on occasion WooCommerce will need to update template files and you
-     * (the theme developer) will need to copy the new files to your theme to
-     * maintain compatibility. We try to do this as little as possible, but it does
-     * happen. When this occurs the version of the template file will be bumped and
-     * the readme will list any important changes.
-     *
-     * @see 	    https://docs.woocommerce.com/document/template-structure/
-     *
-     * @author 		WooThemes
-     *
-     * @version     3.0.0
-     */
-    if (!defined('ABSPATH')) {
-        exit;
-    }
+<?php
+/**
+ * Single Product Up-Sells.
+ *
+ * This template can be overridden by copying it to yourtheme/woocommerce/single-product/up-sells.php.
+ *
+ * HOWEVER, on occasion WooCommerce will need to update template files and you
+ * (the theme developer) will need to copy the new files to your theme to
+ * maintain compatibility. We try to do this as little as possible, but it does
+ * happen. When this occurs the version of the template file will be bumped and
+ * the readme will list any important changes.
+ *
+ * @see 	    https://docs.woocommerce.com/document/template-structure/
+ *
+ * @author 		WooThemes
+ *
+ * @version     3.0.0
+ */
+if (!defined('ABSPATH')) {
+    exit;
+}
 
-    if ($upsells) : ?>
+if ($upsells) : ?>
 
-        <section class="up-sells upsells products">
+    <section class="up-sells upsells products">
 
-            <h2><?php esc_html_e('You may also like&hellip;', 'woocommerce') ?></h2>
+        <h2><?php esc_html_e('You may also like&hellip;', 'woocommerce') ?></h2>
 
-            <?php woocommerce_product_loop_start(); ?>
-                <div class="row">
-                    <?php foreach ($upsells as $upsell) : ?>
-                        <div class="col col-md-6 col-lg-3">
+        <?php woocommerce_product_loop_start(); ?>
+            
+            <?php foreach ($upsells as $upsell) : ?>
+                <div class="col col-md-6 col-lg-3">
 
-                            <?php
-                            $post_object = get_post($upsell->get_id());
+                    <?php
+                    $post_object = get_post($upsell->get_id());
 
-                            setup_postdata($GLOBALS['post'] = &$post_object);
+                    setup_postdata($GLOBALS['post'] = &$post_object);
 
-                            wc_get_template_part('content', 'product'); ?>
+                    wc_get_template_part('content', 'product'); ?>
 
-                        </div>
-                    <?php endforeach; ?>
                 </div>
+            <?php endforeach; ?>
+            
 
-            <?php woocommerce_product_loop_end(); ?>
+        <?php woocommerce_product_loop_end(); ?>
 
-        </section>
+    </section>
 
-    <?php endif;
+<?php endif;
 
-    wp_reset_postdata();
+wp_reset_postdata();
