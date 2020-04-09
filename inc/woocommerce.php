81ae739bc495327898bcf612a2a8c86758d01b0d
info@madeit.be <info@madeit.be>
2020-04-09 21:01:03 +0000

    Fix woocommerce

--- inc/woocommerce.php
+++ inc/woocommerce.php
@@ -1,9 +1,9 @@
-    <?php
+<?php
 
-    if (!function_exists('madeit_loop_columns')) {
-        function madeit_loop_columns()
-        {
-            return 3; // 3 products per row
-        }
-        add_filter('loop_shop_columns', 'madeit_loop_columns', 999);
+if (!function_exists('madeit_loop_columns')) {
+    function madeit_loop_columns()
+    {
+        return 3; // 3 products per row
     }
+    add_filter('loop_shop_columns', 'madeit_loop_columns', 999);
+}
