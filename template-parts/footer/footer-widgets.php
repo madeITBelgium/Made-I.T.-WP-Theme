77e9dfae45de0490fc02c53f645b1036001ee06f
madeITBelgium <madeITBelgium@users.noreply.github.com>
2020-10-25 10:15:58 +0000

    Apply fixes from StyleCI
    [ci skip] [skip ci]

--- template-parts/footer/footer-widgets.php
+++ template-parts/footer/footer-widgets.php
@@ -11,12 +11,18 @@
 <?php
 if (is_active_sidebar('sidebar-2') || is_active_sidebar('sidebar-3') || is_active_sidebar('sidebar-4') || is_active_sidebar('sidebar-5')) {
     $cols = 0;
-    if(is_active_sidebar('sidebar-2')) $cols++;
-    if(is_active_sidebar('sidebar-3')) $cols++;
-    if(is_active_sidebar('sidebar-4')) $cols++;
-    if(is_active_sidebar('sidebar-5')) $cols++;
-    
-    ?>
+    if (is_active_sidebar('sidebar-2')) {
+        $cols++;
+    }
+    if (is_active_sidebar('sidebar-3')) {
+        $cols++;
+    }
+    if (is_active_sidebar('sidebar-4')) {
+        $cols++;
+    }
+    if (is_active_sidebar('sidebar-5')) {
+        $cols++;
+    } ?>
     <div class="container">
         <div class="row" role="complementary" aria-label="<?php esc_attr_e('Footer', 'madeit'); ?>">
             <?php
@@ -27,27 +33,27 @@ if (is_active_sidebar('sidebar-2') || is_active_sidebar('sidebar-3') || is_activ
                 </div>
                 <?php
             }
-            if (is_active_sidebar('sidebar-3')) {
-                ?>
+    if (is_active_sidebar('sidebar-3')) {
+        ?>
                 <div class="col-sm footer-widget-2">
                     <?php dynamic_sidebar('sidebar-3'); ?>
                 </div>
                 <?php
-            }
-            if (is_active_sidebar('sidebar-4')) {
-                ?>
+    }
+    if (is_active_sidebar('sidebar-4')) {
+        ?>
                 <div class="col-sm footer-widget-4">
                     <?php dynamic_sidebar('sidebar-4'); ?>
                 </div>
                 <?php
-            }
-            if (is_active_sidebar('sidebar-5')) {
-                ?>
+    }
+    if (is_active_sidebar('sidebar-5')) {
+        ?>
                 <div class="col-sm footer-widget-5">
                     <?php dynamic_sidebar('sidebar-5'); ?>
                 </div>
                 <?php
-            } ?>
+    } ?>
         </div>
     </div>
     <?php
