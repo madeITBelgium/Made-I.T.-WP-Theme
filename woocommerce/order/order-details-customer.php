88ef2f8d2a4660e8a45dfffde7f317011f1d5f91
tjebbe.lievens@madeit.be <info@madeit.be>
2020-10-25 13:22:23 +0000

    Fix templates

--- woocommerce/order/order-details-customer.php
+++ woocommerce/order/order-details-customer.php
@@ -1,8 +1,8 @@
 <?php
 /**
- * Order Item Details.
+ * Order Customer Details
  *
- * This template can be overridden by copying it to yourtheme/woocommerce/order/order-details-item.php.
+ * This template can be overridden by copying it to yourtheme/woocommerce/order/order-details-customer.php.
  *
  * HOWEVER, on occasion WooCommerce will need to update template files and you
  * (the theme developer) will need to copy the new files to your theme to
@@ -10,58 +10,53 @@
  * happen. When this occurs the version of the template file will be bumped and
  * the readme will list any important changes.
  *
- * @see https://docs.woocommerce.com/document/template-structure/
- *
- * @version 3.7.0
+ * @see     https://docs.woocommerce.com/document/template-structure/
+ * @package WooCommerce\Templates
+ * @version 3.4.4
  */
-if (!defined('ABSPATH')) {
-    exit;
-}
 
-if (!apply_filters('woocommerce_order_item_visible', true, $item)) {
-    return;
-}
+defined( 'ABSPATH' ) || exit;
+
+$show_shipping = ! wc_ship_to_billing_address_only() && $order->needs_shipping_address();
 ?>
-<tr class="<?php echo esc_attr(apply_filters('woocommerce_order_item_class', 'woocommerce-table__line-item order_item', $item, $order)); ?>">
+<section class="woocommerce-customer-details">
 
-    <td class="woocommerce-table__product-name product-name">
-        <?php
-        $is_visible = $product && $product->is_visible();
-        $product_permalink = apply_filters('woocommerce_order_item_permalink', $is_visible ? $product->get_permalink($item) : '', $item, $order);
+	<?php if ( $show_shipping ) : ?>
 
-        echo apply_filters('woocommerce_order_item_name', $product_permalink ? sprintf('<a href="%s">%s</a>', $product_permalink, $item->get_name()) : $item->get_name(), $item, $is_visible); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
+	<section class="woocommerce-columns woocommerce-columns--2 woocommerce-columns--addresses col2-set addresses">
+		<div class="woocommerce-column woocommerce-column--1 woocommerce-column--billing-address col-1">
 
-        $qty = $item->get_quantity();
-        $refunded_qty = $order->get_qty_refunded_for_item($item_id);
+	<?php endif; ?>
 
-        if ($refunded_qty) {
-            $qty_display = '<del>'.esc_html($qty).'</del> <ins>'.esc_html($qty - ($refunded_qty * -1)).'</ins>';
-        } else {
-            $qty_display = esc_html($qty);
-        }
+	<h2 class="woocommerce-column__title"><?php esc_html_e( 'Billing address', 'woocommerce' ); ?></h2>
 
-        echo apply_filters('woocommerce_order_item_quantity_html', ' <strong class="product-quantity">'.sprintf('&times;&nbsp;%s', $qty_display).'</strong>', $item); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
+	<address>
+		<?php echo wp_kses_post( $order->get_formatted_billing_address( esc_html__( 'N/A', 'woocommerce' ) ) ); ?>
 
-        do_action('woocommerce_order_item_meta_start', $item_id, $item, $order, false);
+		<?php if ( $order->get_billing_phone() ) : ?>
+			<p class="woocommerce-customer-details--phone"><?php echo esc_html( $order->get_billing_phone() ); ?></p>
+		<?php endif; ?>
 
-        wc_display_item_meta($item); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
+		<?php if ( $order->get_billing_email() ) : ?>
+			<p class="woocommerce-customer-details--email"><?php echo esc_html( $order->get_billing_email() ); ?></p>
+		<?php endif; ?>
+	</address>
 
-        do_action('woocommerce_order_item_meta_end', $item_id, $item, $order, false);
-        ?>
-    </td>
+	<?php if ( $show_shipping ) : ?>
 
-    <td class="woocommerce-table__product-total product-total">
-        <?php echo $order->get_formatted_line_subtotal($item); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped?>
-    </td>
+		</div><!-- /.col-1 -->
 
-</tr>
+		<div class="woocommerce-column woocommerce-column--2 woocommerce-column--shipping-address col-2">
+			<h2 class="woocommerce-column__title"><?php esc_html_e( 'Shipping address', 'woocommerce' ); ?></h2>
+			<address>
+				<?php echo wp_kses_post( $order->get_formatted_shipping_address( esc_html__( 'N/A', 'woocommerce' ) ) ); ?>
+			</address>
+		</div><!-- /.col-2 -->
 
-<?php if ($show_purchase_note && $purchase_note) { ?>
+	</section><!-- /.col2-set -->
 
-<tr class="woocommerce-table__product-purchase-note product-purchase-note">
+	<?php endif; ?>
 
-    <td colspan="2"><?php echo wpautop(do_shortcode(wp_kses_post($purchase_note))); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped?></td>
+	<?php do_action( 'woocommerce_order_details_after_customer_details', $order ); ?>
 
-</tr>
-
-<?php } ?>
+</section>
