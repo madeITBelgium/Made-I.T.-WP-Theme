387340028cd8be65d76228f07e76e31d4b23d554
madeITBelgium <madeITBelgium@users.noreply.github.com>
2021-01-25 10:45:11 +0000

    Apply fixes from StyleCI
    [ci skip] [skip ci]

--- woocommerce/content-product-cat.php
+++ woocommerce/content-product-cat.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * The template for displaying product category thumbnails within loops
+ * The template for displaying product category thumbnails within loops.
  *
  * This template can be overridden by copying it to yourtheme/woocommerce/content-product-cat.php.
  *
@@ -11,12 +11,11 @@
  * the readme will list any important changes.
  *
  * @see     https://docs.woocommerce.com/document/template-structure/
- * @package WooCommerce\Templates
+ *
  * @version 4.7.0
  */
-
-if ( ! defined( 'ABSPATH' ) ) {
-	exit;
+if (!defined('ABSPATH')) {
+    exit;
 }
 ?>
 <div class="col-12 col-md-6 col-lg-4">
@@ -28,32 +27,30 @@ if ( ! defined( 'ABSPATH' ) ) {
              *
              * @hooked woocommerce_template_loop_category_link_open - 10
              */
-            do_action( 'woocommerce_before_subcategory', $category );
+            do_action('woocommerce_before_subcategory', $category);
 
             /**
              * The woocommerce_before_subcategory_title hook.
              *
              * @hooked woocommerce_subcategory_thumbnail - 10
              */
-            do_action( 'woocommerce_before_subcategory_title', $category );
-
-           
+            do_action('woocommerce_before_subcategory_title', $category);
 
             /**
              * The woocommerce_after_subcategory_title hook.
              */
-            do_action( 'woocommerce_after_subcategory_title', $category );
+            do_action('woocommerce_after_subcategory_title', $category);
 
             ?>
             <div class="card-body text-center d-flex justify-content-between flex-column">
                 <h4 class="card-title">
-                    <a href="<?php echo get_term_link( $category, 'product_cat' ); ?>">
-                        <?php  /**
+                    <a href="<?php echo get_term_link($category, 'product_cat'); ?>">
+                        <?php                         /**
                          * The woocommerce_shop_loop_subcategory_title hook.
                          *
                          * @hooked woocommerce_template_loop_category_title - 10
                          */
-                        do_action( 'woocommerce_shop_loop_subcategory_title', $category ); ?>
+                        do_action('woocommerce_shop_loop_subcategory_title', $category); ?>
                     </a>
                 </h4>
             </div>
