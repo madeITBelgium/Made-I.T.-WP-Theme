291da3fa2d516d98a1822903a5b8a0438a5a3029
info@madeit.be <madeit_dev@server16.ech.be>
2022-12-13 13:47:20 +0000

    Update reviews & Fix menu mobile styling

--- inc/reviews.php
+++ inc/reviews.php
@@ -415,3 +415,25 @@ function list_reviews()
     return ob_get_clean();
 }
 add_shortcode('list_reviews', 'list_reviews');
+
+function review_widget($atts)
+{
+    ob_start();
+
+    ?>
+    <div class="">
+        <div>
+            <svg xmlns="http://www.w3.org/2000/svg" width="35.28" height="36" viewBox="0 0 35.28 36">
+                <path id="__TEMP__SVG__" d="M18.36,15.428V21.6H28.569c-.413,2.647-3.084,7.761-10.209,7.761a11.363,11.363,0,0,1,0-22.722,10.137,10.137,0,0,1,7.178,2.773L30.419,4.7A17.275,17.275,0,0,0,18.36,0a18,18,0,0,0,0,36c10.389,0,17.28-7.3,17.28-17.589a16.5,16.5,0,0,0-.283-2.984h-17Z" transform="translate(-0.36)"/>
+            </svg>
+        </div>
+        <div class="">
+            <h4><?php echo $atts['name']; ?></h4>
+            <p><?php echo $atts['rate']; ?></p>
+        </div>
+    </div>
+    <?php
+
+    return ob_get_clean();
+}
+add_shortcode('review_widget', 'review_widget');
