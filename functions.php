d29be95bf55f5448e364cd415188588ba5635366
Made I.T <info@madeit.be>
2023-12-20 13:34:11 +0100

    Update functions.php

--- functions.php
+++ functions.php
@@ -1873,24 +1873,22 @@ if (!function_exists('madeit_user_analytics')) {
             <?php
         }
 
-        if (defined('MADEIT_ANALYTICS_FB')) {
+        if(defined('MADEIT_ANALYTICS_LINKEDIN')) {
             ?>
-            <!-- Meta Pixel Code -->
-            <script>
-            !function(f,b,e,v,n,t,s)
-            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-            n.queue=[];t=b.createElement(e);t.async=!0;
-            t.src=v;s=b.getElementsByTagName(e)[0];
-            s.parentNode.insertBefore(t,s)}(window, document,'script',
-            'https://connect.facebook.net/en_US/fbevents.js');
-            fbq('init', '<?php echo MADEIT_ANALYTICS_FB; ?>');
-            fbq('track', 'PageView');
+            <script type="text/javascript">
+            _linkedin_partner_id = "<?php echo MADEIT_ANALYTICS_LINKEDIN; ?>";
+            window._linkedin_data_partner_ids = window._linkedin_data_partner_ids || [];
+            window._linkedin_data_partner_ids.push(_linkedin_partner_id);
+            </script><script type="text/javascript">
+            (function(l) {
+            if (!l){window.lintrk = function(a,b){window.lintrk.q.push([a,b])};
+            window.lintrk.q=[]}
+            var s = document.getElementsByTagName("script")[0];
+            var b = document.createElement("script");
+            b.type = "text/javascript";b.async = true;
+            b.src = "https://snap.licdn.com/li.lms-analytics/insight.min.js";
+            s.parentNode.insertBefore(b, s);})(window.lintrk);
             </script>
-            <noscript><img height="1" width="1" style="display:none" src="https://www.facebook.com/tr?id=<?php echo MADEIT_ANALYTICS_FB; ?>&ev=PageView&noscript=1"
-            /></noscript>
-            <!-- End Meta Pixel Code -->
             <?php
         }
 
