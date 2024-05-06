0c0afcda7b49775bd8174f367c4204e509bd01d1
support+a-t-bbe@ech.be <a-t-bbe@server1.ech.be>
2024-05-06 13:13:27 +0000

    Update functions

--- functions.php
+++ functions.php
@@ -1839,19 +1839,16 @@ if (!function_exists('madeit_user_analytics')) {
 
         if (defined('MADEIT_ANALYTICS_TM')) {
             ?>
-            <!-- Google Tag Manager -->
             <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
             new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
             j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
             'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
             })(window,document,'script','dataLayer','<?php echo MADEIT_ANALYTICS_TM; ?>');</script>
-            <!-- End Google Tag Manager -->
             <?php
         }
 
         if (defined('MADEIT_ANALYTICS_FB')) {
             ?>
-            <!-- Meta Pixel Code -->
             <script>
             !function(f,b,e,v,n,t,s)
             {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
@@ -1864,33 +1861,40 @@ if (!function_exists('madeit_user_analytics')) {
             fbq('init', '<?php echo MADEIT_ANALYTICS_FB; ?>');
             fbq('track', 'PageView');
             </script>
-            <noscript><img height="1" width="1" style="display:none" src="https://www.facebook.com/tr?id=<?php echo MADEIT_ANALYTICS_FB; ?>&ev=PageView&noscript=1"
-            /></noscript>
-            <!-- End Meta Pixel Code -->
             <?php
         }
 
-        if (defined('MADEIT_ANALYTICS_FB')) {
+        if(defined('MADEIT_ANALYTICS_LINKEDIN')) {
             ?>
-            <!-- Meta Pixel Code -->
-            <script>
-            !function(f,b,e,v,n,t,s)
-            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-            n.queue=[];t=b.createElement(e);t.async=!0;
-            t.src=v;s=b.getElementsByTagName(e)[0];
-            s.parentNode.insertBefore(t,s)}(window, document,'script',
-            'https://connect.facebook.net/en_US/fbevents.js');
-            fbq('init', '<?php echo MADEIT_ANALYTICS_FB; ?>');
-            fbq('track', 'PageView');
+            <script type="text/javascript">
+            _linkedin_partner_id = "<?php echo MADEIT_ANALYTICS_LINKEDIN; ?>";
+            window._linkedin_data_partner_ids = window._linkedin_data_partner_ids || [];
+            window._linkedin_data_partner_ids.push(_linkedin_partner_id);
+            </script><script type="text/javascript">
+            (function(l) {
+            if (!l){window.lintrk = function(a,b){window.lintrk.q.push([a,b])};
+            window.lintrk.q=[]}
+            var s = document.getElementsByTagName("script")[0];
+            var b = document.createElement("script");
+            b.type = "text/javascript";b.async = true;
+            b.src = "https://snap.licdn.com/li.lms-analytics/insight.min.js";
+            s.parentNode.insertBefore(b, s);})(window.lintrk);
             </script>
-            <noscript><img height="1" width="1" style="display:none" src="https://www.facebook.com/tr?id=<?php echo MADEIT_ANALYTICS_FB; ?>&ev=PageView&noscript=1"
-            /></noscript>
-            <!-- End Meta Pixel Code -->
             <?php
         }
 
+        if(defined('MADEIT_ANALYTICS_TIKTOK')) {
+            ?>
+            <script>
+            !function (w, d, t) {
+            w.TiktokAnalyticsObject=t;var ttq=w[t]=w[t]||[];ttq.methods=["page","track","identify","instances","debug","on","off","once","ready","alias","group","enableCookie","disableCookie"],ttq.setAndDefer=function(t,e){t[e]=function(){t.push([e].concat(Array.prototype.slice.call(arguments,0)))}};for(var i=0;i<ttq.methods.length;i++)ttq.setAndDefer(ttq,ttq.methods[i]);ttq.instance=function(t){for(var e=ttq._i[t]||[],n=0;n<ttq.methods.length;n++)ttq.setAndDefer(e,ttq.methods[n]);return e},ttq.load=function(e,n){var i="https://analytics.tiktok.com/i18n/pixel/events.js";ttq._i=ttq._i||{},ttq._i[e]=[],ttq._i[e]._u=i,ttq._t=ttq._t||{},ttq._t[e]=+new Date,ttq._o=ttq._o||{},ttq._o[e]=n||{};var o=document.createElement("script");o.type="text/javascript",o.async=!0,o.src=i+"?sdkid="+e+"&lib="+t;var a=document.getElementsByTagName("script")[0];a.parentNode.insertBefore(o,a)};
+
+            ttq.load('<?php echo MADEIT_ANALYTICS_TIKTOK; ?>');
+            ttq.page();
+            }(window, document, 'ttq');
+            </script>
+            <?php
+        }
         ?>
         <script>
             (function() {
