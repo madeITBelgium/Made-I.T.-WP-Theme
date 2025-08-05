bb90dd1dc1630b04619267d8846f8655f2af3bfb
Made I.T. <info@madeit.be>
2025-08-05 12:47:20 +0200

    Update cart-empty.php

--- woocommerce/cart/cart-empty.php
+++ woocommerce/cart/cart-empty.php
@@ -16,24 +16,25 @@
  *
  * @version 7.0.1
  */
-if (!defined('ABSPATH')) {
-    exit; // Exit if accessed directly
-}
+defined( 'ABSPATH' ) || exit;
 
 /*
  * @hooked wc_empty_cart_message - 10
  */
-do_action('woocommerce_cart_is_empty');
-?>
+do_action( 'woocommerce_cart_is_empty' );
 
-<h2 class="text-center empty-shopping-cart-message"><?php echo apply_filters( 'wc_empty_cart_message', __( 'Your cart is currently empty.', 'woocommerce' ) ); ?></h2>
-
-
-<?php
-if (wc_get_page_id('shop') > 0) { ?>
+if ( wc_get_page_id( 'shop' ) > 0 ) : ?>
 	<p class="return-to-shop">
-		<a class="button wc-backward<?php echo esc_attr(wc_wp_theme_get_element_class_name('button') ? ' '.wc_wp_theme_get_element_class_name('button') : ''); ?>" href="<?php echo esc_url(apply_filters('woocommerce_return_to_shop_redirect', wc_get_page_permalink('shop'))); ?>">
-			<?php _e('Return to shop', 'woocommerce') ?>
+		<a class="button wc-backward<?php echo esc_attr( wc_wp_theme_get_element_class_name( 'button' ) ? ' ' . wc_wp_theme_get_element_class_name( 'button' ) : '' ); ?>" href="<?php echo esc_url( apply_filters( 'woocommerce_return_to_shop_redirect', wc_get_page_permalink( 'shop' ) ) ); ?>">
+			<?php
+				/**
+				 * Filter "Return To Shop" text.
+				 *
+				 * @since 4.6.0
+				 * @param string $default_text Default text.
+				 */
+				echo esc_html( apply_filters( 'woocommerce_return_to_shop_text', __( 'Return to shop', 'woocommerce' ) ) );
+			?>
 		</a>
 	</p>
-<?php } ?>
+<?php endif; ?>
