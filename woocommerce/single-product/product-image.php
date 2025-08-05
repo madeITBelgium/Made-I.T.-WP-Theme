0cc2a059429ea9f05af24078a4d29f8ad76dcfe7
info@madeit.be <madeit_dev@server16.ech.be>
2025-08-05 11:32:32 +0000

    Update

--- woocommerce/single-product/product-image.php
+++ woocommerce/single-product/product-image.php
@@ -12,8 +12,10 @@
  *
  * @see     https://woocommerce.com/document/template-structure/
  * @package WooCommerce\Templates
- * @version 9.0.0
+ * @version 9.7.0
  */
+
+use Automattic\WooCommerce\Enums\ProductType;
 
 defined( 'ABSPATH' ) || exit;
 
@@ -27,32 +29,32 @@ global $product;
 $columns           = apply_filters( 'woocommerce_product_thumbnails_columns', 4 );
 $post_thumbnail_id = $product->get_image_id();
 $wrapper_classes   = apply_filters(
-	'woocommerce_single_product_image_gallery_classes',
-	array(
-		'woocommerce-product-gallery',
-		'woocommerce-product-gallery--' . ( $post_thumbnail_id ? 'with-images' : 'without-images' ),
-		'woocommerce-product-gallery--columns-' . absint( $columns ),
-		'images',
-	)
+    'woocommerce_single_product_image_gallery_classes',
+    array(
+        'woocommerce-product-gallery',
+        'woocommerce-product-gallery--' . ( $post_thumbnail_id ? 'with-images' : 'without-images' ),
+        'woocommerce-product-gallery--columns-' . absint( $columns ),
+        'images',
+    )
 );
 ?>
 <div class="<?php echo esc_attr( implode( ' ', array_map( 'sanitize_html_class', $wrapper_classes ) ) ); ?>" data-columns="<?php echo esc_attr( $columns ); ?>" style="opacity: 0; transition: opacity .25s ease-in-out;">
-	<div class="woocommerce-product-gallery__wrapper">
-		<?php
-		if ( $post_thumbnail_id ) {
-			$html = wc_get_gallery_image_html( $post_thumbnail_id, true );
-		} else {
-			$wrapper_classname = $product->is_type( 'variable' ) && ! empty( $product->get_available_variations( 'image' ) ) ?
-				'woocommerce-product-gallery__image woocommerce-product-gallery__image--placeholder' :
-				'woocommerce-product-gallery__image--placeholder';
-			$html              = sprintf( '<div class="%s">', esc_attr( $wrapper_classname ) );
-			$html             .= sprintf( '<img src="%s" alt="%s" class="wp-post-image" />', esc_url( wc_placeholder_img_src( 'woocommerce_single' ) ), esc_html__( 'Awaiting product image', 'woocommerce' ) );
-			$html             .= '</div>';
-		}
+    <div class="woocommerce-product-gallery__wrapper">
+        <?php
+        if ( $post_thumbnail_id ) {
+            $html = wc_get_gallery_image_html( $post_thumbnail_id, true );
+        } else {
+            $wrapper_classname = $product->is_type( ProductType::VARIABLE ) && ! empty( $product->get_available_variations( 'image' ) ) ?
+                'woocommerce-product-gallery__image woocommerce-product-gallery__image--placeholder' :
+                'woocommerce-product-gallery__image--placeholder';
+            $html              = sprintf( '<div class="%s">', esc_attr( $wrapper_classname ) );
+            $html             .= sprintf( '<img src="%s" alt="%s" class="wp-post-image" />', esc_url( wc_placeholder_img_src( 'woocommerce_single' ) ), esc_html__( 'Awaiting product image', 'woocommerce' ) );
+            $html             .= '</div>';
+        }
 
-		echo apply_filters( 'woocommerce_single_product_image_thumbnail_html', $html, $post_thumbnail_id ); // phpcs:disable WordPress.XSS.EscapeOutput.OutputNotEscaped
+        echo apply_filters( 'woocommerce_single_product_image_thumbnail_html', $html, $post_thumbnail_id ); // phpcs:disable WordPress.XSS.EscapeOutput.OutputNotEscaped
 
-		do_action( 'woocommerce_product_thumbnails' );
-		?>
-	</div>
+        do_action( 'woocommerce_product_thumbnails' );
+        ?>
+    </div>
 </div>
--- woocommerce/single-product/tabs/tabs.php
+++ woocommerce/single-product/tabs/tabs.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Single Product tabs.
+ * Single Product tabs
  *
  * This template can be overridden by copying it to yourtheme/woocommerce/single-product/tabs/tabs.php.
  *
@@ -10,11 +10,12 @@
  * happen. When this occurs the version of the template file will be bumped and
  * the readme will list any important changes.
  *
- * @see     https://docs.woocommerce.com/document/template-structure/
- *
- * @version 3.8.0
+ * @see     https://woocommerce.com/document/template-structure/
+ * @package WooCommerce\Templates
+ * @version 9.8.0
  */
-if (!defined('ABSPATH')) {
+
+if ( ! defined( 'ABSPATH' ) ) {
     exit;
 }
 
@@ -25,31 +26,31 @@ if (!defined('ABSPATH')) {
  *
  * @see woocommerce_default_product_tabs()
  */
-$product_tabs = apply_filters('woocommerce_product_tabs', []);
+$product_tabs = apply_filters( 'woocommerce_product_tabs', array() );
 
-if (!empty($product_tabs)) { ?>
+if ( ! empty( $product_tabs ) ) : ?>
 
-	<div class="woocommerce-tabs wc-tabs-wrapper">
-		<ul class="tabs wc-tabs" role="tablist">
-			<?php foreach ($product_tabs as $key => $product_tab) { ?>
-				<li class="<?php echo esc_attr($key); ?>_tab" id="tab-title-<?php echo esc_attr($key); ?>" role="tab" aria-controls="tab-<?php echo esc_attr($key); ?>">
-					<a href="#tab-<?php echo esc_attr($key); ?>">
-						<?php echo wp_kses_post(apply_filters('woocommerce_product_'.$key.'_tab_title', $product_tab['title'], $key)); ?>
-					</a>
-				</li>
-			<?php } ?>
-		</ul>
-		<?php foreach ($product_tabs as $key => $product_tab) { ?>
-			<div class="woocommerce-Tabs-panel woocommerce-Tabs-panel--<?php echo esc_attr($key); ?> panel entry-content wc-tab" id="tab-<?php echo esc_attr($key); ?>" role="tabpanel" aria-labelledby="tab-title-<?php echo esc_attr($key); ?>">
-				<?php
-                if (isset($product_tab['callback'])) {
-                    call_user_func($product_tab['callback'], $key, $product_tab);
+    <div class="woocommerce-tabs wc-tabs-wrapper">
+        <ul class="tabs wc-tabs" role="tablist">
+            <?php foreach ( $product_tabs as $key => $product_tab ) : ?>
+                <li role="presentation" class="<?php echo esc_attr( $key ); ?>_tab" id="tab-title-<?php echo esc_attr( $key ); ?>">
+                    <a href="#tab-<?php echo esc_attr( $key ); ?>" role="tab" aria-controls="tab-<?php echo esc_attr( $key ); ?>">
+                        <?php echo wp_kses_post( apply_filters( 'woocommerce_product_' . $key . '_tab_title', $product_tab['title'], $key ) ); ?>
+                    </a>
+                </li>
+            <?php endforeach; ?>
+        </ul>
+        <?php foreach ( $product_tabs as $key => $product_tab ) : ?>
+            <div class="woocommerce-Tabs-panel woocommerce-Tabs-panel--<?php echo esc_attr( $key ); ?> panel entry-content wc-tab" id="tab-<?php echo esc_attr( $key ); ?>" role="tabpanel" aria-labelledby="tab-title-<?php echo esc_attr( $key ); ?>">
+                <?php
+                if ( isset( $product_tab['callback'] ) ) {
+                    call_user_func( $product_tab['callback'], $key, $product_tab );
                 }
                 ?>
-			</div>
-		<?php } ?>
+            </div>
+        <?php endforeach; ?>
 
-		<?php do_action('woocommerce_product_after_tabs'); ?>
-	</div>
+        <?php do_action( 'woocommerce_product_after_tabs' ); ?>
+    </div>
 
-<?php } ?>
+<?php endif; ?>
