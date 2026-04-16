1aa003cbad3c7554726c631b1f0a6e858840c9c0
Tjebbe Lievens <tjebbe@madeit.be>
2026-04-16 08:47:37 +0000

    Update card block

--- gutenberg/blocks/madeit-card/build/index.asset.php
+++ gutenberg/blocks/madeit-card/build/index.asset.php
@@ -1,3 +1,1 @@
-<?php
-
-return ['dependencies' => ['react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-i18n', 'wp-keycodes'], 'version' => '4c90c1d18b1b56d19147'];
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-i18n', 'wp-keycodes'), 'version' => 'be27149b652acb0c1bc4');
