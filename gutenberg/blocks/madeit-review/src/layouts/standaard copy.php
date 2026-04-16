d393ac5746b75403fa4ada6bed5b9b0f295d18b3
Tjebbe Lievens <tjebbe@madeit.be>
2026-04-16 15:27:46 +0000

    Improvements

--- gutenberg/blocks/madeit-review/src/layouts/standaard copy.php
+++ gutenberg/blocks/madeit-review/src/layouts/standaard copy.php
@@ -1,18 +1,7 @@
-<?php if ($show_image && $avatar_url !== '') : ?>
-	<div class="madeit-review-card__avatar">
-		<img
-			src="<?php echo esc_url($avatar_url); ?>"
-			alt="<?php echo esc_attr($avatar_alt !== '' ? $avatar_alt : $naam); ?>"
-			style="<?php echo esc_attr(implode(';', $img_styles)); ?>"
-			loading="lazy"
-		/>
-	</div>
-<?php endif; ?>
+
 
 <div class="madeit-review-card__content">
-	<?php if ($titel !== '') : ?>
-		<div class="madeit-review-card__title"><?php echo esc_html($titel); ?></div>
-	<?php endif; ?>
+	
 
 	<?php if ($rating > 0) : ?>
 		<div class="madeit-review-card__rating" aria-label="<?php echo esc_attr(sprintf(__('Rating: %d van 5', 'madeit-review'), $rating)); ?>">
