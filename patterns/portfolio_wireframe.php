b062f0e49a2df893fdffb4169356eef3b7c6ac37
Tjebbe Lievens <tjebbe@madeit.be>
2026-05-05 13:50:57 +0000

    Update

---
+++ patterns/portfolio_wireframe.php
@@ -0,0 +1,251 @@
+<?php
+/**
+ * Title: Portfolio Wireframe
+ * Slug: madeit/portfolio-wireframe
+ * Post Types: page
+ */
+?>
+
+<!-- wp:madeit/block-content {"backgroundType":"classic","containerBackgroundImage":{"id":2532,"url":"https://test.madeit.be/wp-content/uploads/2026/05/placeholder.png","alt":""},"containerBackgroundPosition":"center center","containerBackgroundRepeat":"no-repeat","containerBackgroundSize":"cover","size":"container-fluid","contentWidth":"container","containerPaddingOnRow":true,"overflow":"visible","flexDirection":"row","flexDirectionTablet":"column","flexDirectionMobile":"column","alignItems":"center","justifyContent":"flex-start","minHeight":100,"minHeightUnit":"vh","minHeightUnitTablet":"vh","rowGap":20,"rowGapTablet":20,"rowGapMobile":20,"columnsCount":1,"flexWrap":"nowrap","madeitHasUserEdits":true} -->
+<div class="wp-block-madeit-block-content container-fluid madeit-block-content--frontend" style="background-image:url(https://test.madeit.be/wp-content/uploads/2026/05/placeholder.png);background-position:center center;background-repeat:no-repeat;background-size:cover;--madeit-min-height-desktop:100vh;--madeit-align-items-desktop:center"><div class="container"><div class="row madeit-container-row rows-1" data-madeit-dir="row" data-madeit-dir-tablet="column" data-madeit-dir-mobile="column">
+
+<!-- wp:madeit/block-content-column -->
+<div class="wp-block-madeit-block-content-column col-12 col-lg-12"><div class="madeit-content-column__inner"><!-- wp:heading {"textAlign":"center","level":1,"madeitTypoClass":"madeit-typo-d2d5beb"} -->
+<h1 class="wp-block-heading has-text-align-center">This Headline Grabs Visitors’ Attention</h1>
+<!-- /wp:heading -->
+
+<!-- wp:spacer {"height":"20px"} -->
+<div style="height:20px" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer -->
+
+<!-- wp:paragraph {"align":"center","fontSize":"h5","madeitTypoClass":"madeit-typo-e0f1ccf"} -->
+<p class="has-text-align-center has-h-5-font-size">A short description introducing visitors to your portfolio and the services you offer</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:spacer {"height":"30px"} -->
+<div style="height:30px" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer -->
+
+<!-- wp:buttons {"layout":{"type":"flex","justifyContent":"center"}} -->
+<div class="wp-block-buttons"><!-- wp:button -->
+<div class="wp-block-button"><a class="wp-block-button__link wp-element-button">Contacteer ons</a></div>
+<!-- /wp:button --></div>
+<!-- /wp:buttons --></div></div>
+<!-- /wp:madeit/block-content-column -->
+
+</div></div></div>
+<!-- /wp:madeit/block-content -->
+
+<!-- wp:madeit/block-content {"contentWidth":"container","containerMargin":{"top":"100px","bottom":"100px"},"flexDirectionTablet":"column","flexDirectionMobile":"column","alignItems":"center","justifyContent":"center","columnsCount":2} -->
+<div class="wp-block-madeit-block-content container madeit-block-content--frontend" style="--madeit-align-items-desktop:center;--madeit-container-margin-top-desktop:100px;--madeit-container-margin-bottom-desktop:100px"><div class="container"><div class="row madeit-container-row rows-2" data-madeit-dir="row" data-madeit-dir-tablet="column" data-madeit-dir-mobile="column">
+
+<!-- wp:madeit/block-content-column {"width":6} -->
+<div class="wp-block-madeit-block-content-column col-12 col-lg-6"><div class="madeit-content-column__inner"><!-- wp:heading {"madeitTypoClass":"madeit-typo-1af6b76"} -->
+<h2 class="wp-block-heading">Your Name</h2>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph {"style":{"typography":{"textTransform":"uppercase"}},"madeitTypoClass":"madeit-typo-9745b13"} -->
+<p style="text-transform:uppercase">Your Professional Title</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:spacer {"height":"30px"} -->
+<div style="height:30px" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer -->
+
+<!-- wp:paragraph {"madeitTypoClass":"madeit-typo-7038206"} -->
+<p>Use this section to share your story, your experience, or the skills you possess. This is an opportunity to create a connection with the visitor and give them the confidence that you have the ability to provide the service they need.</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:spacer {"height":"30px"} -->
+<div style="height:30px" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer -->
+
+<!-- wp:buttons -->
+<div class="wp-block-buttons"><!-- wp:button -->
+<div class="wp-block-button"><a class="wp-block-button__link wp-element-button">Contacteer ons</a></div>
+<!-- /wp:button --></div>
+<!-- /wp:buttons --></div></div>
+<!-- /wp:madeit/block-content-column -->
+
+<!-- wp:madeit/block-content-column {"width":6} -->
+<div class="wp-block-madeit-block-content-column col-12 col-lg-6"><div class="madeit-content-column__inner"><!-- wp:image {"sizeSlug":"large"} -->
+<figure class="wp-block-image size-large"><img src="https://placehold.net/default.png" alt=""/></figure>
+<!-- /wp:image --></div></div>
+<!-- /wp:madeit/block-content-column -->
+
+</div></div></div>
+<!-- /wp:madeit/block-content -->
+
+<!-- wp:madeit/block-content {"contentWidth":"container","containerMargin":{"top":"100px","bottom":"100px"},"flexDirectionTablet":"column","flexDirectionMobile":"column","justifyContent":"center","columnsCount":1} -->
+<div class="wp-block-madeit-block-content container madeit-block-content--frontend" style="--madeit-justify-content-desktop:center;--madeit-container-margin-top-desktop:100px;--madeit-container-margin-bottom-desktop:100px"><div class="container"><div class="row madeit-container-row rows-1" data-madeit-dir="row" data-madeit-dir-tablet="column" data-madeit-dir-mobile="column">
+
+<!-- wp:madeit/block-content-column -->
+<div class="wp-block-madeit-block-content-column col-12 col-lg-12"><div class="madeit-content-column__inner"><!-- wp:heading {"textAlign":"center","madeitTypoClass":"madeit-typo-06e591c"} -->
+<h2 class="wp-block-heading has-text-align-center">Recent projects</h2>
+<!-- /wp:heading -->
+
+<!-- wp:heading {"textAlign":"center","level":5,"madeitTypoClass":"madeit-typo-cf1cb41"} -->
+<h5 class="wp-block-heading has-text-align-center">A short description that introduces visitors to your portfolio</h5>
+<!-- /wp:heading -->
+
+<!-- wp:spacer {"height":"40px"} -->
+<div style="height:40px" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer -->
+
+<!-- wp:madeit/block-content {"contentWidth":"container","containerPaddingOnRow":true,"overflow":"visible","flexDirection":"row","flexDirectionTablet":"column","flexDirectionMobile":"column","alignItems":"stretch","justifyContent":"flex-start","rowGap":20,"rowGapTablet":20,"rowGapMobile":20,"columnsCount":3,"flexWrap":"nowrap"} -->
+<div class="wp-block-madeit-block-content container madeit-block-content--frontend"><div class="container"><div class="row madeit-container-row rows-3" data-madeit-dir="row" data-madeit-dir-tablet="column" data-madeit-dir-mobile="column">
+
+<!-- wp:madeit/block-content-column {"width":4} -->
+<div class="wp-block-madeit-block-content-column col-12 col-lg-4"><div class="madeit-content-column__inner"><!-- wp:image {"id":2532,"sizeSlug":"full","linkDestination":"none"} -->
+<figure class="wp-block-image size-full"><img src="https://test.madeit.be/wp-content/uploads/2026/05/placeholder.png" alt="" class="wp-image-2532"/></figure>
+<!-- /wp:image -->
+
+<!-- wp:spacer {"height":"20px"} -->
+<div style="height:20px" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer -->
+
+<!-- wp:heading {"textAlign":"center","level":4,"madeitTypoClass":"madeit-typo-71f071c"} -->
+<h4 class="wp-block-heading has-text-align-center">Project Title</h4>
+<!-- /wp:heading --></div></div>
+<!-- /wp:madeit/block-content-column -->
+
+<!-- wp:madeit/block-content-column {"width":4} -->
+<div class="wp-block-madeit-block-content-column col-12 col-lg-4"><div class="madeit-content-column__inner"><!-- wp:image {"id":2532,"sizeSlug":"full","linkDestination":"none"} -->
+<figure class="wp-block-image size-full"><img src="https://test.madeit.be/wp-content/uploads/2026/05/placeholder.png" alt="" class="wp-image-2532"/></figure>
+<!-- /wp:image -->
+
+<!-- wp:spacer {"height":"20px"} -->
+<div style="height:20px" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer -->
+
+<!-- wp:heading {"textAlign":"center","level":4,"madeitTypoClass":"madeit-typo-71f071c"} -->
+<h4 class="wp-block-heading has-text-align-center">Project Title</h4>
+<!-- /wp:heading --></div></div>
+<!-- /wp:madeit/block-content-column -->
+
+<!-- wp:madeit/block-content-column {"width":4} -->
+<div class="wp-block-madeit-block-content-column col-12 col-lg-4"><div class="madeit-content-column__inner"><!-- wp:image {"id":2532,"sizeSlug":"full","linkDestination":"none"} -->
+<figure class="wp-block-image size-full"><img src="https://test.madeit.be/wp-content/uploads/2026/05/placeholder.png" alt="" class="wp-image-2532"/></figure>
+<!-- /wp:image -->
+
+<!-- wp:spacer {"height":"20px"} -->
+<div style="height:20px" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer -->
+
+<!-- wp:heading {"textAlign":"center","level":4,"madeitTypoClass":"madeit-typo-71f071c"} -->
+<h4 class="wp-block-heading has-text-align-center">Project Title</h4>
+<!-- /wp:heading --></div></div>
+<!-- /wp:madeit/block-content-column -->
+
+</div></div></div>
+<!-- /wp:madeit/block-content -->
+
+<!-- wp:spacer {"height":"40px"} -->
+<div style="height:40px" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer -->
+
+<!-- wp:buttons {"layout":{"type":"flex","justifyContent":"center"}} -->
+<div class="wp-block-buttons"><!-- wp:button -->
+<div class="wp-block-button"><a class="wp-block-button__link wp-element-button">View all projects</a></div>
+<!-- /wp:button --></div>
+<!-- /wp:buttons --></div></div>
+<!-- /wp:madeit/block-content-column -->
+
+</div></div></div>
+<!-- /wp:madeit/block-content -->
+
+<!-- wp:madeit/block-content {"backgroundType":"classic","customContainerBackgroundColor":"#e3e5e6","size":"container-fluid","contentWidth":"container","containerMargin":{"top":"100px","bottom":"100px"},"containerPadding":{"top":"50px","bottom":"50px"},"containerPaddingOnRow":true,"flexDirectionTablet":"column","flexDirectionMobile":"column","justifyContent":"center","columnsCount":1,"madeitHasUserEdits":true} -->
+<div class="wp-block-madeit-block-content container-fluid madeit-block-content--frontend" style="background-color:#e3e5e6;--madeit-justify-content-desktop:center;--madeit-container-margin-top-desktop:100px;--madeit-container-margin-bottom-desktop:100px"><div class="container"><div class="row madeit-container-row rows-1" data-madeit-dir="row" data-madeit-dir-tablet="column" data-madeit-dir-mobile="column" style="--madeit-container-row-padding-top-desktop:50px;--madeit-container-row-padding-right-desktop:0px;--madeit-container-row-padding-bottom-desktop:50px;--madeit-container-row-padding-left-desktop:0px">
+
+<!-- wp:madeit/block-content-column -->
+<div class="wp-block-madeit-block-content-column col-12 col-lg-12"><div class="madeit-content-column__inner"><!-- wp:heading {"textAlign":"center","madeitTypoClass":"madeit-typo-06e591c"} -->
+<h2 class="wp-block-heading has-text-align-center">As Featured In</h2>
+<!-- /wp:heading -->
+
+<!-- wp:heading {"textAlign":"center","level":5,"madeitTypoClass":"madeit-typo-cf1cb41"} -->
+<h5 class="wp-block-heading has-text-align-center">This is a space to showcase any publications your work has been featured in</h5>
+<!-- /wp:heading -->
+
+<!-- wp:spacer {"height":"40px"} -->
+<div style="height:40px" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer -->
+
+<!-- wp:madeit/slider {"effect":"logo","slidesDesktop":5,"slidesTablet":3.5,"slidesMobile":2.5,"height":"200px","minHeight":"100px","spaceBetween":"50px","objectFit":"contain","speed":14500} -->
+<div class="wp-block-madeit-slider m-slider_front" data-slides-desktop="5" data-slides-tablet="3.5" data-slides-mobile="2.5" data-space-between="50px" data-speed="14500" data-autoplay="true" data-loop="false" data-navigation="false" data-pagination="false" data-touch-slides="true" data-effect="logo" data-cross-fade="false" data-pagination-type="false" style="--height:200px;--spaceBetween:50px;--minheight:100px;--swiper-space-between:50px;--navigationIconSize:20px;--paginationActiveColor:#000000;--objectFit:contain"><div class="swiper" style="overflow:hidden;height:200px;min-height:100px"><div class="swiper-wrapper"><!-- wp:madeit/slider-image {"url":"https://test.madeit.be/wp-content/uploads/2026/05/Partners-Logo_5-13122021.svg","alt":"","id":2581} -->
+<div class="wp-block-madeit-slider-image swiper-slide"><img src="https://test.madeit.be/wp-content/uploads/2026/05/Partners-Logo_5-13122021.svg" alt=""/></div>
+<!-- /wp:madeit/slider-image -->
+
+<!-- wp:madeit/slider-image {"url":"https://test.madeit.be/wp-content/uploads/2026/05/Partners-Logo_2-13122021.svg","alt":"","id":2582} -->
+<div class="wp-block-madeit-slider-image swiper-slide"><img src="https://test.madeit.be/wp-content/uploads/2026/05/Partners-Logo_2-13122021.svg" alt=""/></div>
+<!-- /wp:madeit/slider-image -->
+
+<!-- wp:madeit/slider-image {"url":"https://test.madeit.be/wp-content/uploads/2026/05/Partners-Logo_1-13122021.svg","alt":"","id":2583} -->
+<div class="wp-block-madeit-slider-image swiper-slide"><img src="https://test.madeit.be/wp-content/uploads/2026/05/Partners-Logo_1-13122021.svg" alt=""/></div>
+<!-- /wp:madeit/slider-image -->
+
+<!-- wp:madeit/slider-image {"url":"https://test.madeit.be/wp-content/uploads/2026/05/Partners-Logo_4-13122021.svg","alt":"","id":2584} -->
+<div class="wp-block-madeit-slider-image swiper-slide"><img src="https://test.madeit.be/wp-content/uploads/2026/05/Partners-Logo_4-13122021.svg" alt=""/></div>
+<!-- /wp:madeit/slider-image -->
+
+<!-- wp:madeit/slider-image {"url":"https://test.madeit.be/wp-content/uploads/2026/05/Partners-Logo_3-13122021.svg","alt":"","id":2585} -->
+<div class="wp-block-madeit-slider-image swiper-slide"><img src="https://test.madeit.be/wp-content/uploads/2026/05/Partners-Logo_3-13122021.svg" alt=""/></div>
+<!-- /wp:madeit/slider-image --></div></div></div>
+<!-- /wp:madeit/slider --></div></div>
+<!-- /wp:madeit/block-content-column -->
+
+</div></div></div>
+<!-- /wp:madeit/block-content -->
+
+<!-- wp:madeit/block-content {"contentWidth":"container","flexDirectionTablet":"column","flexDirectionMobile":"column","justifyContent":"center","columnsCount":1} -->
+<div class="wp-block-madeit-block-content container madeit-block-content--frontend" style="--madeit-justify-content-desktop:center"><div class="container"><div class="row madeit-container-row rows-1" data-madeit-dir="row" data-madeit-dir-tablet="column" data-madeit-dir-mobile="column">
+
+<!-- wp:madeit/block-content-column -->
+<div class="wp-block-madeit-block-content-column col-12 col-lg-12"><div class="madeit-content-column__inner"><!-- wp:heading {"textAlign":"center","madeitTypoClass":"madeit-typo-06e591c"} -->
+<h2 class="wp-block-heading has-text-align-center">Client Testimonials</h2>
+<!-- /wp:heading -->
+
+<!-- wp:spacer {"height":"40px"} -->
+<div style="height:40px" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer -->
+
+<!-- wp:madeit/reviews {"layout":"modern"} /--></div></div>
+<!-- /wp:madeit/block-content-column -->
+
+</div></div></div>
+<!-- /wp:madeit/block-content -->
+
+<!-- wp:madeit/block-content {"backgroundType":"classic","customContainerBackgroundColor":"#dcdfe3","containerBackgroundPosition":"right center","containerBackgroundRepeat":"no-repeat","containerBackgroundSize":"cover","size":"container-fluid","contentWidth":"container","containerMargin":{"top":"100px"},"containerPadding":{"top":"100px","bottom":"100px"},"containerPaddingOnRow":true,"flexDirectionTablet":"row","flexDirectionMobile":"column-reverse","alignItems":"center","justifyContentTablet":"center","columnsCount":2,"madeitHasUserEdits":true} -->
+<div class="wp-block-madeit-block-content container-fluid madeit-block-content--frontend" style="background-color:#dcdfe3;--madeit-flex-direction-tablet:row;--madeit-flex-direction-mobile:column-reverse;--madeit-align-items-desktop:center;--madeit-justify-content-tablet:center;--madeit-container-margin-top-desktop:100px"><div class="container"><div class="row madeit-container-row rows-2" data-madeit-dir="row" data-madeit-dir-tablet="row" data-madeit-dir-mobile="column-reverse" style="--madeit-container-row-padding-top-desktop:100px;--madeit-container-row-padding-right-desktop:0px;--madeit-container-row-padding-bottom-desktop:100px;--madeit-container-row-padding-left-desktop:0px">
+
+<!-- wp:madeit/block-content-column {"width":6} -->
+<div class="wp-block-madeit-block-content-column col-12 col-lg-6"><div class="madeit-content-column__inner"><!-- wp:heading {"madeitTypoClass":"madeit-typo-4de2ecb"} -->
+<h2 class="wp-block-heading">A Title That Gets the Visitor to Contact You</h2>
+<!-- /wp:heading -->
+
+<!-- wp:spacer {"height":"20px"} -->
+<div style="height:20px" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer -->
+
+<!-- wp:paragraph {"madeitTypoClass":"madeit-typo-1fad99e"} -->
+<p>This is your chance to emphasize why the visitor should contact you right now.</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:spacer {"height":"30px"} -->
+<div style="height:30px" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer -->
+
+<!-- wp:buttons -->
+<div class="wp-block-buttons"><!-- wp:button -->
+<div class="wp-block-button"><a class="wp-block-button__link wp-element-button">Contact us now</a></div>
+<!-- /wp:button --></div>
+<!-- /wp:buttons --></div></div>
+<!-- /wp:madeit/block-content-column -->
+
+<!-- wp:madeit/block-content-column {"width":6} -->
+<div class="wp-block-madeit-block-content-column col-12 col-lg-6"><div class="madeit-content-column__inner"><!-- wp:image {"id":2532,"sizeSlug":"full","linkDestination":"none"} -->
+<figure class="wp-block-image size-full"><img src="https://test.madeit.be/wp-content/uploads/2026/05/placeholder.png" alt="" class="wp-image-2532"/></figure>
+<!-- /wp:image --></div></div>
+<!-- /wp:madeit/block-content-column -->
+
+</div></div></div>
+<!-- /wp:madeit/block-content -->
---
+++ performance/performance.php
@@ -0,0 +1,291 @@
+<?php
+
+/**
+ * Performance monitoring for WordPress hooks using Query Monitor.
+ * This plugin adds start and stop actions for various WordPress hooks to measure their execution time in Query Monitor.
+ * Plugin Name:       Performance Monitoring for WordPress Hooks
+ * Plugin URI:        https://www.madeit.be
+ * Description:       Adds performance monitoring for various WordPress hooks using Query Monitor.
+ * Version:           6.8.0
+ * Author:            Made I.T.
+ * Text Domain:       madeit
+ * Requires PHP:      7.4
+ * Requires at least: 6.2
+ */
+
+function madeit_perf_mu_plugin_loaded_start()
+{
+    do_action('qm/start', 'madeit:perf_mu_plugin_loaded');
+}
+add_action('mu_plugin_loaded', 'madeit_perf_mu_plugin_loaded_start', 0);
+
+function madeit_perf_mu_plugin_loaded_end()
+{
+    do_action('qm/stop', 'madeit:perf_mu_plugin_loaded');
+}
+add_action('mu_plugin_loaded', 'madeit_perf_mu_plugin_loaded_end', PHP_INT_MAX);
+
+function madeit_perf_network_plugin_loaded_start()
+{
+    do_action('qm/start', 'madeit:perf_network_plugin_loaded');
+}
+add_action('network_plugin_loaded', 'madeit_perf_network_plugin_loaded_start', 0);
+
+function madeit_perf_network_plugin_loaded_end()
+{
+    do_action('qm/stop', 'madeit:perf_network_plugin_loaded');
+}
+add_action('network_plugin_loaded', 'madeit_perf_network_plugin_loaded_end', PHP_INT_MAX);
+
+function madeit_perf_muplugins_loaded_start()
+{
+    do_action('qm/start', 'madeit:perf_muplugins_loaded');
+}
+add_action('muplugins_loaded', 'madeit_perf_muplugins_loaded_start', 0);
+
+function madeit_perf_muplugins_loaded_end()
+{
+    do_action('qm/stop', 'madeit:perf_muplugins_loaded');
+}
+add_action('muplugins_loaded', 'madeit_perf_muplugins_loaded_end', PHP_INT_MAX);
+/*
+function madeit_perf_registered_taxonomy_start()
+{
+    do_action('qm/start', 'madeit:perf_registered_taxonomy');
+}
+add_action('registered_taxonomy', 'madeit_perf_registered_taxonomy_start', 0);
+
+function madeit_perf_registered_taxonomy_end()
+{
+    do_action('qm/stop', 'madeit:perf_registered_taxonomy');
+}
+add_action('registered_taxonomy', 'madeit_perf_registered_taxonomy_end', PHP_INT_MAX);
+
+function madeit_perf_registered_post_type_start()
+{
+    do_action('qm/start', 'madeit:perf_registered_post_type');
+}
+add_action('registered_post_type', 'madeit_perf_registered_post_type_start', 0);
+
+function madeit_perf_registered_post_type_end()
+{
+    do_action('qm/stop', 'madeit:perf_registered_post_type');
+}
+add_action('registered_post_type', 'madeit_perf_registered_post_type_end', PHP_INT_MAX);
+*/
+function madeit_perf_plugin_loaded_start($plugin)
+{
+    do_action('qm/start', 'madeit:perf_plugin_loaded_' . $plugin);
+}
+add_action('plugin_loaded', 'madeit_perf_plugin_loaded_start', 0, 1);
+
+function madeit_perf_plugin_loaded_end($plugin)
+{
+    do_action('qm/stop', 'madeit:perf_plugin_loaded_' . $plugin);
+}
+add_action('plugin_loaded', 'madeit_perf_plugin_loaded_end', PHP_INT_MAX, 1);
+
+function madeit_perf_plugins_loaded_start()
+{
+    do_action('qm/start', 'madeit:perf_plugins_loaded');
+}
+add_action('plugins_loaded', 'madeit_perf_plugins_loaded_start', 0);
+
+function madeit_perf_plugins_loaded_end()
+{
+    do_action('qm/stop', 'madeit:perf_plugins_loaded');
+}
+add_action('plugins_loaded', 'madeit_perf_plugins_loaded_end', PHP_INT_MAX);
+
+function madeit_perf_setup_theme_start()
+{
+    do_action('qm/start', 'madeit:perf_setup_theme');
+}
+add_action('setup_theme', 'madeit_perf_setup_theme_start', 0);
+
+function madeit_perf_setup_theme_end()
+{
+    do_action('qm/stop', 'madeit:perf_setup_theme');
+}
+add_action('setup_theme', 'madeit_perf_setup_theme_end', PHP_INT_MAX);
+
+function madeit_perf_load_textdomain_start()
+{
+    do_action('qm/start', 'madeit:perf_load_textdomain');
+}
+add_action('load_textdomain', 'madeit_perf_load_textdomain_start', 0);
+
+function madeit_perf_load_textdomain_end()
+{
+    do_action('qm/stop', 'madeit:perf_load_textdomain');
+}
+add_action('load_textdomain', 'madeit_perf_load_textdomain_end', PHP_INT_MAX);
+
+function madeit_perf_after_setup_theme_start()
+{
+    do_action('qm/start', 'madeit:perf_after_setup_theme');
+}
+add_action('after_setup_theme', 'madeit_perf_after_setup_theme_start', 0);
+
+function madeit_perf_after_setup_theme_end()
+{
+    do_action('qm/stop', 'madeit:perf_after_setup_theme');
+}
+add_action('after_setup_theme', 'madeit_perf_after_setup_theme_end', PHP_INT_MAX);
+
+function madeit_perf_init_start()
+{
+    do_action('qm/start', 'madeit:perf_init');
+}
+add_action('init', 'madeit_perf_init_start', 0);
+
+function madeit_perf_init_end()
+{
+    do_action('qm/stop', 'madeit:perf_init');
+}
+add_action('init', 'madeit_perf_init_end', PHP_INT_MAX);
+
+function madeit_perf_widgets_init_start()
+{
+    do_action('qm/start', 'madeit:perf_widgets_init');
+}
+add_action('widgets_init', 'madeit_perf_widgets_init_start', 0);
+
+function madeit_perf_widgets_init_end()
+{
+    do_action('qm/stop', 'madeit:perf_widgets_init');
+}
+add_action('widgets_init', 'madeit_perf_widgets_init_end', PHP_INT_MAX);
+
+function madeit_perf_register_sidebar_start()
+{
+    do_action('qm/start', 'madeit:perf_register_sidebar');
+}
+add_action('register_sidebar', 'madeit_perf_register_sidebar_start', 0);
+
+function madeit_perf_register_sidebar_end()
+{
+    do_action('qm/stop', 'madeit:perf_register_sidebar');
+}
+add_action('register_sidebar', 'madeit_perf_register_sidebar_end', PHP_INT_MAX);
+
+function madeit_perf_wp_loaded_start()
+{
+    do_action('qm/start', 'madeit:perf_wp_loaded');
+}
+add_action('wp_loaded', 'madeit_perf_wp_loaded_start', 0);
+
+function madeit_perf_wp_loaded_end()
+{
+    do_action('qm/stop', 'madeit:perf_wp_loaded');
+}
+add_action('wp_loaded', 'madeit_perf_wp_loaded_end', PHP_INT_MAX);
+
+function madeit_perf_send_headers_start()
+{
+    do_action('qm/start', 'madeit:perf_send_headers');
+}
+add_action('send_headers', 'madeit_perf_send_headers_start', 0);
+
+function madeit_perf_send_headers_end()
+{
+    do_action('qm/stop', 'madeit:perf_send_headers');
+}
+add_action('send_headers', 'madeit_perf_send_headers_end', PHP_INT_MAX);
+
+function madeit_perf_wp_start()
+{
+    do_action('qm/start', 'madeit:perf_wp');
+}
+add_action('wp', 'madeit_perf_wp_start', 0);
+
+function madeit_perf_wp_end()
+{
+    do_action('qm/stop', 'madeit:perf_wp');
+}
+add_action('wp', 'madeit_perf_wp_end', PHP_INT_MAX);
+
+function madeit_perf_template_redirect_start()
+{
+    do_action('qm/start', 'madeit:perf_template_redirect');
+}
+add_action('template_redirect', 'madeit_perf_template_redirect_start', 0);
+
+function madeit_perf_template_redirect_end()
+{
+    do_action('qm/stop', 'madeit:perf_template_redirect');
+}
+add_action('template_redirect', 'madeit_perf_template_redirect_end', PHP_INT_MAX);
+
+function madeit_perf_wp_head_start()
+{
+    do_action('qm/start', 'madeit:perf_wp_head');
+}
+add_action('wp_head', 'madeit_perf_wp_head_start', 0);
+
+function madeit_perf_wp_head_end()
+{
+    do_action('qm/stop', 'madeit:perf_wp_head');
+}
+add_action('wp_head', 'madeit_perf_wp_head_end', PHP_INT_MAX);
+
+function madeit_perf_wp_footer_start()
+{
+    do_action('qm/start', 'madeit:perf_wp_footer');
+}
+add_action('wp_footer', 'madeit_perf_wp_footer_start', 0);
+
+function madeit_perf_wp_footer_end()
+{
+    do_action('qm/stop', 'madeit:perf_wp_footer');
+}
+add_action('wp_footer', 'madeit_perf_wp_footer_end', PHP_INT_MAX);
+
+function madeit_perf_shutdown_start()
+{
+    do_action('qm/start', 'madeit:perf_shutdown');
+}
+add_action('shutdown', 'madeit_perf_shutdown_start', 0);
+
+function madeit_perf_shutdown_end()
+{
+    do_action('qm/stop', 'madeit:perf_shutdown');
+}
+add_action('shutdown', 'madeit_perf_shutdown_end', PHP_INT_MAX-1);
+
+/*
+send_headers
+parse_query
+pre_get_posts — WP main query.
+posts_clauses
+posts_selection
+pre_handle_404
+send_headers — additional HTTP headers: caching, type, etc.
+wp
+template_redirect — before plugging in the template file.
+
+
+
+get_header
+wp_head
+wp_enqueue_scripts
+wp_print_styles
+wp_print_scripts
+
+get_search_form
+loop_start
+the_post
+get_template_part_content
+loop_end
+get_sidebar
+dynamic_sidebar
+pre_get_comments
+wp_meta
+
+get_footer
+wp_footer
+wp_print_footer_scripts
+admin_bar_menu
+wp_before_admin_bar_render
+wp_after_admin_bar_render
+*/
